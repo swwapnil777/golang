b5bfe5c2602fc029d6c5f97a10dc73f62c85ec31
Swapnil Khatik <swapnilkhatik777@gmail.com>
2024-08-28 19:45:35 +0530

    Add for loop in go

---
+++ 5_for/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// for --> only construct in go for looping
+
+func main() {
+
+	// while loop
+
+	// i := 1
+
+	// for i <= 10 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	//classic for loop
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+}
